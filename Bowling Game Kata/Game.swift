c2469d76771d934d5ffeece2c1b02ce3c05a70c4
Michael Borgmann <michaelborgmann@mailbox.org>
2016-07-06 21:12:52 +0200

    test spare, strike, and full game

--- Bowling Game Kata/Game.swift
+++ Bowling Game Kata/Game.swift
@@ -11,15 +11,37 @@ import Foundation
 class Game {
     
     var totalScore = 0
-    var rolls = Array(repeating: 0, count: 21)
-    var currentRoll = 0
+    var rolls = [Int]()
+    var frameTotal = 0
 
     func roll(pinsDown: Int) {
-        rolls.insert(pinsDown, at: currentRoll)
+        rolls.append(pinsDown)
+        if pinsDown == 10 && rolls.count % 2 == 1 {
+            rolls.append(0)
+        }
     }
     
     func score() -> Int {
-        return rolls.reduce(0, combine: +)
+        for (index, points) in rolls.enumerated() {
+            
+            if index < 20 {
+                totalScore += points
+            }
+            
+            if index % 2 == 0 {
+                frameTotal = points
+                if frameTotal == 10 {
+                    totalScore += rolls[index+2] + rolls[index+3]
+                }
+            } else {
+                frameTotal += points
+                if frameTotal == 10 && rolls[index-1] < 10 {
+                    totalScore += rolls[index+1]
+                }
+            }
+        }
+        
+        return totalScore
     }
     
 }
--- Bowling Game KataTests/Bowling_Game_KataTests.swift
+++ Bowling Game KataTests/Bowling_Game_KataTests.swift
@@ -36,5 +36,64 @@ class Bowling_Game_KataTests: XCTestCase {
         }
         XCTAssertEqual(game.score(), 20)
     }
+ 
+    func testKnockDownAllPinsWithTwoThrowsInFrameIsSpareAndAddsPointsFromNextThrow() {
+        game.roll(pinsDown: 6)
+        game.roll(pinsDown: 4)
+        game.roll(pinsDown: 5)
+        game.roll(pinsDown: 4)
+        XCTAssertEqual(game.score(), 24)
+    }
     
+    
+    func testKnockDownAllPinsWithFirstThrowInFrameIsStrikeAndAddsPointsFromNextTwoThrows() {
+        game.roll(pinsDown: 10)
+        game.roll(pinsDown: 1)
+        game.roll(pinsDown: 1)
+        game.roll(pinsDown: 1)
+        XCTAssertEqual(game.score(), 15)
+    }
+    
+    func testFullGame() {
+        // 1. frame (5)
+        game.roll(pinsDown: 1)
+        game.roll(pinsDown: 4)
+        
+        // 2. frame (14 = 5 + 9)
+        game.roll(pinsDown: 4)
+        game.roll(pinsDown: 5)
+        
+        // 3. frame (29 = 14 + 10 + 5)
+        game.roll(pinsDown: 6)
+        game.roll(pinsDown: 4)
+        
+        // 4. frame (49 = 29 + 10 + 10)
+        game.roll(pinsDown: 5)
+        game.roll(pinsDown: 5)
+        
+        // 5. frame (60 = 49 + 10 + 1)
+        game.roll(pinsDown: 10)
+        
+        // 6. frame (61 = 60 + 1)
+        game.roll(pinsDown: 0)
+        game.roll(pinsDown: 1)
+        
+        // 7. frame (77 = 61 + 10 + 6)
+        game.roll(pinsDown: 7)
+        game.roll(pinsDown: 3)
+        
+        // 8. frame (97 = 77 + 10 + 10)
+        game.roll(pinsDown: 6)
+        game.roll(pinsDown: 4)
+        
+        // 9. frame (117 = 97 + 10 + 10)
+        game.roll(pinsDown: 10)
+        
+        // 10. frame (133 = 117 + 10 + 6)
+        game.roll(pinsDown: 2)
+        game.roll(pinsDown: 8)
+        game.roll(pinsDown: 6)
+                
+        XCTAssertEqual(game.score(), 133)
+    }
 }
